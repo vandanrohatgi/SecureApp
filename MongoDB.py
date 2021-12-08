0481596783cc5e4464ce943a79e35e672b30d7d2
vandanrohatgi <vandanrohatgi@gmail.com>
2021-12-08 11:29:14 +0530

    completed registration module

--- MongoDB.py
+++ MongoDB.py
@@ -4,7 +4,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 class database():
     def __init__(self):
-        with open("connectionString.txt",'r') as f:
+        with open("connectionString.txt",'r') as f:  # mongoDB connection string is stored someone secure and read from there
             ConnectionString=f.read()
         client=MongoClient(ConnectionString)
         db=client['SecureApp']
@@ -20,11 +20,11 @@ class database():
         return item
 
     def updateData(self,oldName=None,newName=None,oldPassword=None,newPassword=None):
-        if newName and self.query(oldName):
+        if newName and oldName:
             self.collection.update_one(filter={'username':oldName},update={"$set":{'username':newName}})
             return 200,"success!"
         
-        if newPassword and oldPassword:
+        elif newPassword and oldPassword:
             current=self.query(oldName)['password']
             correct=check_password_hash(current,oldPassword)
             if correct:
@@ -32,13 +32,12 @@ class database():
                 return 200,"success!"
             else:
                 return (500,"Old password incorrect")
-        elif newPassword and not oldPassword:
-            return(500,"Old password required")
-        elif oldPassword and not newPassword:
-            return(500,"New password required")
-   
-    def register(user,password,self):
-        pass
+
+        elif newName and newPassword:
+            newCreds={"username":newName,"password":generate_password_hash(newPassword)}
+            self.collection.insert_one(newCreds)
+            return 201,"user registered!"
+
 
 #obj=database()
 #print(obj.query("adin"))
--- main.py
+++ main.py
@@ -6,14 +6,19 @@ from werkzeug.security import check_password_hash
 
 
 app=Flask(__name__,static_url_path='',static_folder='web/static',template_folder='web/templates')
-app.secret_key=os.urandom(12) 
-app.env="development"
+app.secret_key=os.urandom(12)  # secure secret key
+app.env="development" # will be changing these lines when finished development
 app.debug=True
 
 db=database()
 
+def isInputValid(data):
+    result=False
+    if re.search("^[a-zA-Z0-9]*$",data) != None:
+        result=True
+    return result
 
-@app.before_request
+@app.before_request  # check if logged in before every request
 def before_request():
     g.user=None
     if 'user' in session:
@@ -27,7 +32,7 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
         record=db.query(username)
-        if record!=None and check_password_hash(record['password'],password):
+        if record!=None and check_password_hash(record['password'],password): # check and compare hash of entered password with the one in database
             session['user']=username
             return redirect(url_for('welcome'))
         return render_template('index.html',error="Username and/or password incorrect")
@@ -58,12 +63,13 @@ def profile():
 def update():
     if not g.user:
         return redirect(url_for('login'))
+
     if request.method=='POST':
         action=request.form.get('action')
         try:
             if int(action)==1:
                 newName=request.form.get('newName')
-                if not re.search("^[a-zA-Z0-9]*$",newName):
+                if not isInputValid(newName): # allow only alphabets and numbers in the username
                     return ("Only alphabets and numbers please!",500)
                 try:
                     status,message=db.updateData(oldName=session['user'],newName=newName)
@@ -71,23 +77,48 @@ def update():
                     return render_template('profile.html',message=message),status
                 except:
                     return "Something went wrong while updating database",500
+
             elif int(action)==2:
                 oldPassword=request.form.get('oldPassword')
                 newPassword=request.form.get('newPassword')
+                if not oldPassword or not newPassword:
+                    return "Both passwords please!",500
                 try:
                     status,message=db.updateData(oldName=session['user'],oldPassword=oldPassword,newPassword=newPassword)
                     return render_template('profile.html',message=message),status
                 except:
                     return  "Something went wrong while updating database",500
+
             else:
-                return render_template('profile.html',error="You shouldn't have tampered with that. Security services are on their way..."),500
+                return render_template('profile.html',error="You shouldn't have tampered with that. Security services are on their way..."),500  #since the "action" parameter is hidden, it shouldn't change unless someone tampers with it
+
         except:
             return 'It wasn\'t me...what did you do?!', 500
     else:
         return redirect(url_for('profile'))
 
-# create method to check for special chars
-#     
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        session.pop('user', None)
+        newUsername = request.form.get('newUsername')
+        newPassword = request.form.get('newPassword')
+        print(newPassword,newUsername)
+        if not newUsername or not newPassword:
+            return "Fill both fields please!",500
+        record=db.query(newUsername)
+        if record==None:
+            if not isInputValid(newUsername):
+                return "Alphabets and numbers only please!",500
+            
+            try:
+                status,message=db.updateData(newName=newUsername,newPassword=newPassword)
+                return render_template("index.html",error=message)
+            except:
+                return "Something went wrong while registering you!",500
+        else:
+            return "username already exists!",500
+    return render_template('register.html')  
     
 
 if __name__ == "__main__":
